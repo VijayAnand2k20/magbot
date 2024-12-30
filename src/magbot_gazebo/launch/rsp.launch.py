8fa7b4dc27f21867de915f42ca03f45fdaa4197c
Vijay Anand <vijayanand2k20@gmail.com>
2024-12-30 15:56:15 +0530

    Bug in ros2 control

--- src/magbot_gazebo/launch/rsp.launch.py
+++ src/magbot_gazebo/launch/rsp.launch.py
@@ -355,6 +355,8 @@
 #         # load_robot_controller,
 #         spawn_robot,
 #     ])
+
+############################################
 import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler, ExecuteProcess, TimerAction
@@ -368,26 +370,14 @@ from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Declare arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    
-    # Paths to required files
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     urdf_file = os.path.join(get_package_share_directory('magbot_gazebo'), 'description', 'urdf', 'dingo.urdf.xacro')
-    # world_file = os.path.join(get_package_share_directory('magbot_gazebo'), 'description', 'worlds', 'normal.world')
     
-    # Robot description from xacro
+    
     robot_description = ParameterValue(Command([
         FindExecutable(name='xacro'), ' ', urdf_file
     ]), value_type=str)
-
-    # Gazebo launch file
-    # gazebo = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
-    #     ),
-    #     # launch_arguments={'world': world_file, 'use_sim_time': use_sim_time}.items()
-    # )
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
@@ -395,70 +385,43 @@ def generate_launch_description():
         launch_arguments={'verbose': 'true'}.items()
     )
     
-    # Node to spawn the robot in Gazebo
+    
     spawn_robot = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        # namespace='dingo_controller',
         arguments=['-topic', 'robot_description', '-entity', 'magbot', '-z', '0.2'],
         output='screen'
     )
-    # spawn_robot = ExecuteProcess(
-    #     cmd=[
-    #         'ros2', 'run', 'gazebo_ros', 'spawn_entity.py',
-    #         '-entity', 'magbot',
-    #         '-file', urdf_file,
-    #         '-z', '3',
-    #         '-J', 'FR_theta2', '0.72646626',
-    #         '-J', 'FL_theta2', '0.72646626',
-    #         '-J', 'RR_theta2', '0.72646626',
-    #         '-J', 'RL_theta2', '0.72646626',
-    #         '-J', 'FR_theta1', '0.0',
-    #         '-J', 'FL_theta1', '0.0',
-    #         '-J', 'RR_theta1', '0.0',
-    #         '-J', 'RL_theta1', '0.0',
-    #         '-J', 'FR_theta3', '0.0',
-    #         '-J', 'FL_theta3', '0.0',
-    #         '-J', 'RR_theta3', '0.0',
-    #         '-J', 'RL_theta3', '0.0'
-    #     ],
-    #     output='screen'
-    # )
-
-    # controller_manager = Node(
-    #     package='controller_manager',
-    #     executable='ros2_control_node',
-    #     namespace='dingo_controller',
-    #     parameters=[PathJoinSubstitution([
-    #         FindPackageShare('magbot_gazebo'), 'config', 'dingo_controllers.yaml'
-    #     ])],
-    #     output='screen',
-    # )
 
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        # namespace='dingo_controller',
-        # arguments=['joint_state_broadcaster'],
+        parameters=[
+                {"use_sim_time": True}
+        ],
         arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
         output='screen'
     )
 
-    # Spawner node to activate the dingo_controller
     dingo_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        # namespace='dingo_controller',
         arguments=['dingo_controller', '--controller-manager', '/controller_manager'],
         output='screen'
     )
+    load_controllers = ExecuteProcess(
+        cmd=[
+            'ros2', 'control', 'load_start_controller', 
+            '--all', 
+            '--set-state', 'active', 
+        ],
+        output='screen'
+    )
 
-    # Robot State Publisher
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        # namespace='dingo_controller',
         output='screen',
         parameters=[{'publish_frequency': 10.0,
                      'robot_description': robot_description,
@@ -471,26 +434,6 @@ def generate_launch_description():
         robot_state_publisher,
         gazebo,
         spawn_robot,
-
-        # controller_manager,
-        # RegisterEventHandler(
-        #     event_handler=OnProcessStart(
-        #         target_action=controller_manager,
-        #         on_start=[
-        #             # Add a delay before spawning controllers
-        #             TimerAction(
-        #                 period=5.0,
-        #                 actions=[
-        #                     joint_state_broadcaster_spawner,
-        #                     dingo_controller_spawner
-        #                 ]
-        #             )
-        #         ]
-        #     )
-        # ),
-
-
         joint_state_broadcaster_spawner,
         dingo_controller_spawner,
-        # load_robot_controller,
     ])
