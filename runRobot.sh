766e98977837b234c9b88ae9a5377e7b879286a9
Vijay Anand <vijayanand2k20@gmail.com>
2025-03-01 19:54:50 +0530

    Added slam_toolbox params

--- runRobot.sh
+++ runRobot.sh
@@ -11,3 +11,5 @@ ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 world odom
 ros2 run magbot_input_interfacing magbot_keyboard_interfacing
 
 ros2 run rviz2 rviz2
+
+ros2 launch slam_toolbox online_async_launch.py params_file:=./src/magbot_gazebo/config/mapper_params_online_async.yaml use_sim_time:=true
