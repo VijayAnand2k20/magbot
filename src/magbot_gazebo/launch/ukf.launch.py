beea521c94606fa4e1a3656c8483036eaf4050b5
Vijay Anand <vijayanand2k20@gmail.com>
2025-03-30 09:22:47 +0530

    Added EKF and UKF launch files

--- src/magbot_gazebo/launch/ukf.launch.py
+++ src/magbot_gazebo/launch/ukf.launch.py
@@ -5,19 +5,19 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    ekf_config_path = os.path.join(
+    ukf_config_path = os.path.join(
         get_package_share_directory('magbot_gazebo'),
         'config',
-        'ekf.yaml'
+        'ukf.yaml'
     )
 
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='true', description='Use simulation time'),
         Node(
             package='robot_localization',
-            executable='ekf_node',
-            name='ekf_filter_node',
+            executable='ukf_node',
+            name='ukf_filter_node',
             output='screen',
-            parameters=[ekf_config_path, {'use_sim_time': True}],
+            parameters=[ukf_config_path, {'use_sim_time': True}],
         )
     ])
